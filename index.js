54c4e6773dcf93d6d8ba53f73a680c9777dd29ce
Jannis R <mail@jannisr.de>
2016-05-11 00:08:29 +0200

    minor stuff

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const config       = require('config')
 
 const api = express()
 
+const onError = (req, res, err) => {
+	console.error(err.message)
+	res.status(500).json({error: true, msg: err.message})
+	return err
+}
+
+
+
 api.get('/stations', (req, res) => {
 	if (!req.query.query) return res.status(400).end('Missing query.')
 	if (req.query.completion === 'true')
@@ -17,7 +25,7 @@ api.get('/stations', (req, res) => {
 	else {
 		res.type('application/x-ndjson')
 		search(req.query.query)
-		.on('error', (err) => res.status(500).end(err.message))
+		.on('error', (err) => onError(req, res, err))
 		.pipe(ndjson.stringify())
 		.pipe(res)
 	}
@@ -25,6 +33,7 @@ api.get('/stations', (req, res) => {
 
 api.get('/stations/:id/departures', (req, res) => {
 	hafas.departures(config.vbbKey, req.params.id)
+	.catch((err) => (err) => onError(req, res, err))
 	.then((deps) => res.json(deps))
 })
 
@@ -32,8 +41,10 @@ api.get('/routes', (req, res) => {
 	if (!req.query.from) return res.status(400).end('Missing origin station.')
 	if (!req.query.to) return res.status(400).end('Missing destination station.')
 	hafas.routes(config.vbbKey, req.query.from, req.query.to)
-	.catch((err) => res.status(500).end(err.message))
+	.catch((err) => (err) => onError(req, res, err))
 	.then((routes) => res.json(routes))
 })
+
+
 
 api.listen(3000)
