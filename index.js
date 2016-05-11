5c71a8157067144718bb5de55326e43eb3a79649
Jannis R <mail@jannisr.de>
2016-05-11 12:33:34 +0200

    rate-limiting

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 'use strict'
 
+const redis        = require('redis')
 const express      = require('express')
+const limiter      = require('express-limiter')
 const autocomplete = require('vbb-stations-autocomplete')
 const search       = require('vbb-find-stations')
 const ndjson       = require('ndjson')
 const hafas        = require('vbb-hafas')
 const config       = require('config')
 
+const db = redis.createClient()
 const api = express()
+
+const limit = ((tracker) => (amount) => tracker({
+	  lookup: 'connection.remoteAddress'
+	, total:  amount
+	, expire: 10 * 60 * 1000 // 10min
+}))(limiter(api, db))
 
 const onError = (req, res, err) => {
 	console.error(err.message)
@@ -17,7 +26,7 @@ const onError = (req, res, err) => {
 
 
 
-api.get('/stations', (req, res) => {
+api.get('/stations', limit(1000), (req, res) => {
 	if (!req.query.query) return res.status(400).end('Missing query.')
 	if (req.query.completion === 'true')
 		return res.json(autocomplete(req.query.query)
@@ -31,13 +40,13 @@ api.get('/stations', (req, res) => {
 	}
 })
 
-api.get('/stations/:id/departures', (req, res) => {
+api.get('/stations/:id/departures', limit(250), (req, res) => {
 	hafas.departures(config.vbbKey, req.params.id)
 	.catch((err) => onError(req, res, err))
 	.then((deps) => res.json(deps))
 })
 
-api.get('/routes', (req, res) => {
+api.get('/routes', limit(100), (req, res) => {
 	if (!req.query.from) return res.status(400).end('Missing origin station.')
 	if (!req.query.to) return res.status(400).end('Missing destination station.')
 	hafas.routes(config.vbbKey, req.query.from, req.query.to)
