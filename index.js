ae45819c9ef4ec7fb85983eafdb88fccf504904b
Jannis R <mail@jannisr.de>
2018-11-21 00:19:48 +0100

    health check route, minor fixes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const parse  = require('cli-native').to
 const createHafas = require('vbb-hafas')
 const createApi = require('hafas-rest-api')
+const createHealthCheck = require('hafas-client-health-check')
 
 const pkg = require('./package.json')
 const attachMiddleware = require('./api')
@@ -34,22 +35,26 @@ const addHafasOpts = (opt, method, req) => {
 
 const config = {
 	hostname: process.env.HOSTNAME || '2.vbb.transport.rest',
-	port: process.env.PORT || 3000,
+	port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
 	name: pkg.name,
 	description: pkg.description,
+	version: pkg.version,
 	homepage: pkg.homepage,
 	docsLink: 'https://github.com/derhuerst/vbb-rest/blob/2/docs/index.md',
 	logging: true,
 	aboutPage: true,
 	addHafasOpts
 }
+const berlinFriedrichstr = '900000100001'
 
 const api = createApi(config)
 
 pHafas
 .then((hafas) => {
-	const api = createApi(hafas, config, attachMiddleware)
+	const cfg = Object.assign(Object.create(null), config)
+	cfg.healthCheck = createHealthCheck(hafas, berlinFriedrichstr)
 
+	const api = createApi(hafas, cfg, attachMiddleware)
 	api.listen(config.port, (err) => {
 		if (err) return showError(err)
 		else console.info(`Listening on ${config.hostname}:${config.port}.`)
