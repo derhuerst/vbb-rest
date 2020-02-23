0db4e2b35a0e04a1be034025702013c70472ff0d
Jannis R <mail@jannisr.de>
2020-02-23 09:41:31 +0000

    default hostname: localhost, minor changes

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const addHafasOpts = (opt, method, req) => {
 }
 
 const config = {
-	hostname: process.env.HOSTNAME || '3.vbb.transport.rest',
+	hostname: process.env.HOSTNAME || 'localhost',
 	port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
 	name: pkg.name,
 	description: pkg.description,
