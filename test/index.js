0d5db65330686ec7c233db255ec48db495e3352f
Jannis R <mail@jannisr.de>
2022-11-22 16:59:41 +0100

    better tests for /stations & /stations?query ✅
    vbb-rest equivalent of derhuerst/db-rest@f982e15

--- test/index.js
+++ test/index.js
@@ -1,44 +1,73 @@
 'use strict'
 
 const tape = require('tape')
+const {parse: ndjsonParser} = require('ndjson')
 const {fetchWithTestApi} = require('./util')
+const {data: allStations} = require('../lib/vbb-stations')
 
 tape.test('/stations works', async (t) => {
+	const someStationId = Object.keys(allStations)[0]
+
 	{
-		const {headers} = await fetchWithTestApi({}, '/stations', {
+		const {headers, data} = await fetchWithTestApi({}, '/stations', {
 			headers: {
 				'accept': 'application/json',
 			},
 		})
 		t.equal(headers['content-type'], 'application/json')
+		t.equal(typeof data, 'object')
+		t.ok(data)
+		t.ok(data[someStationId])
+		t.equal(Object.keys(data).length, Object.keys(allStations).length)
 	}
 
 	{
-		const {headers} = await fetchWithTestApi({}, '/stations', {
+		const {headers, data} = await fetchWithTestApi({}, '/stations', {
 			headers: {
 				'accept': 'application/x-ndjson',
 			},
 		})
 		t.equal(headers['content-type'], 'application/x-ndjson')
+
+		let nrOfStations = 0
+		const parser = ndjsonParser()
+		parser.end(data)
+		for await (const station of parser) nrOfStations++
+
+		t.equal(nrOfStations, Object.keys(allStations).length)
 	}
 })
 
 tape.test('/stations?query=hauptbah works', async (t) => {
+	const BERLIN_HBF = '900000003201'
+
 	{
-		const {headers} = await fetchWithTestApi({}, '/stations?query=hauptbah', {
+		const {headers, data} = await fetchWithTestApi({}, '/stations?query=hauptbah', {
 			headers: {
 				'accept': 'application/json',
 			},
 		})
 		t.equal(headers['content-type'], 'application/json')
+		t.equal(typeof data, 'object')
+		t.ok(data)
+		t.ok(data[BERLIN_HBF])
+		t.ok(Object.keys(data).length > 0)
 	}
 
 	{
-		const {headers} = await fetchWithTestApi({}, '/stations?query=hauptbah', {
+		const {headers, data} = await fetchWithTestApi({}, '/stations?query=hauptbah', {
 			headers: {
 				'accept': 'application/x-ndjson',
 			},
 		})
 		t.equal(headers['content-type'], 'application/x-ndjson')
+
+		const stations = []
+		const parser = ndjsonParser()
+		parser.end(data)
+		for await (const station of parser) stations.push(station)
+
+		t.ok(stations.find(s => s.id === BERLIN_HBF))
+		t.ok(Object.keys(stations).length > 0)
 	}
 })
